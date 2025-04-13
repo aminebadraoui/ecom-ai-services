cb8d0b9d58faeec50ae41764ae9fff2e13db4c3a
Amine Badraoui <aminebadraoui94@gmail.com>
2025-04-13 18:23:08 -0400

    fix redis protection

--- app/core/config.py
+++ app/core/config.py
@@ -13,6 +13,21 @@ class Settings(BaseSettings):
     REDIS_HOST: str = "redis"
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
+    REDIS_PASSWORD: Optional[str] = os.environ.get("REDIS_PASSWORD")
+    
+    @property
+    def REDIS_URL(self) -> str:
+        if self.REDIS_PASSWORD:
+            return f"redis://:{self.REDIS_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+    
+    @property
+    def CELERY_BROKER_URL(self) -> str:
+        return self.REDIS_URL
+    
+    @property
+    def CELERY_RESULT_BACKEND(self) -> str:
+        return self.REDIS_URL
     
     class Config:
         env_file = ".env"
